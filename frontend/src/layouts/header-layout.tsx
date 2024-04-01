f1e92ac7454b49676f413f954d3009a23df4c3fa
Mohitsen11 <mohitsen0102@gmail.com>
2024-04-02 01:08:17 +0530

    refactor: Created hero component to separate main slogan/message

--- frontend/src/layouts/header-layout.tsx
+++ frontend/src/layouts/header-layout.tsx
@@ -2,6 +2,7 @@ import bg from '@/assets/wanderlustbg.webp';
 import ThemeToggle from '@/components/theme-toggle-button';
 import AddIcon from '@/assets/svg/add-icon-white.svg';
 import { useNavigate } from 'react-router-dom';
+import Hero from '@/components/hero';
 function header() {
     const navigate = useNavigate();
 
@@ -38,14 +39,7 @@ function header() {
             </button>
             </div>
             </div>
-            <div className="mb-8 flex max-w-3xl flex-1 flex-col justify-end text-slate-50">
-            <h1 className="text-xl font-bold md:text-4xl">Journey Beyond Horizons</h1>
-            <p className="my-4 text-base md:text-lg">
-                Dive into the world of travel with stories that transport you to  far-off lands.
-                Adventure awaits around every corner. It's time to explore the world!
-            </p>
-            <div className="text-sm font-semibold md:text-xl">Let's go!</div>
-            </div>
+            <Hero/>
         </div>
         </div>
     )
